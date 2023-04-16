ff6f6f92427467d1f65005987f5059cc3eac598f
Nica Tudor <zatalo.nox@gmail.com>
2023-04-16 14:05:25 +0300

    Upload the calculator

---
+++ calculator.js
@@ -0,0 +1,12 @@
+// This will be a calculator programed in JavaScript
+
+let a = parseFloat(prompt("Enter the first number: "));
+let b = parseFloat(prompt("Enter the second number: "));
+
+//Results
+
+console.log("The results are...");
+console.log("Addition: " + (a + b));
+console.log("Subtraction: " + (a - b));
+console.log("Multiplication: " + (a * b));
+console.log("Division: " + (a / b));
